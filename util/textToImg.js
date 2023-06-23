4ccb92d757aad75b2e669a6a3f5819eeca63820a
sunsunmonkey <2933201543@qq.com>
2023-06-23 18:31:21 +0800

    small-bug

--- util/textToImg.js
+++ util/textToImg.js
@@ -5,14 +5,15 @@ export const textToImg ={
         "阴":"./images/day/yun.png",
         "雨":"./images/day/yu.png",
         "日落":"./images/set.png",
-        
+        "日出":"./images/rise.png",
     },
     night:{
         "多云":"./images/night/yin.png",
         "晴":"./images/night/qing.png",
         "阴":"./images/night/yun.png",
         "雨":"./images/night/yu.png",
-        "日出":"./images/rise.png"
+        "日出":"./images/rise.png",
+        "日落":"./images/set.png",
     },
     indicate:{
         2:"./images/washcar.png",
